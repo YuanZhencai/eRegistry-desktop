a372f0ed9060074fd2dc3c8731b08c0a41b5e345
yuanzhencai <zhencai.yuan@datarx.cn>
2020-03-04 10:24:35 +0800

    feat: member questionnaire

--- src/renderer/views/project/ProjectMember.vue
+++ src/renderer/views/project/ProjectMember.vue
@@ -1,54 +1,59 @@
 <template>
-    <div class='app-container'>
-        <el-row>
-            <el-col :span='24'>
-                <el-button type='primary' class='float-right' @click="newMember()">添加成员</el-button>
-            </el-col>
-        </el-row>
-        <el-row>
-            <el-table v-loading="loading" stripe :data='members.slice((currentPage-1)*pageSize, currentPage*pageSize)'
-                      :default-sort = "{prop: 'id', order: 'descending'}" style='width: 100%'>
-                <el-table-column prop='id' label='ID' width='180' sortable></el-table-column>
-                <el-table-column label='用户名' width='180' sortable>
-                    <template slot-scope="scope">
-                        {{scope.row.username}}
-                        <span v-if="project && scope.row.username === project.chargedBy">(负责人)</span>
-                    </template>
-                </el-table-column>
-                <el-table-column prop='centerName' label='所属中心' sortable></el-table-column>
-                <el-table-column label='任务'>
-                    <template slot-scope="scope">{{ taskType[scope.row.task] }}</template>
-                </el-table-column>
-                <el-table-column label="操作">
-                    <template slot-scope="scope" >
-                        <el-button type="text" @click="assignCenter(scope.row)">分配中心</el-button>
-                        <el-divider direction="vertical"></el-divider>
-                        <el-button type="text" @click="assignTask(scope.row)">分配任务</el-button>
-                        <el-divider direction="vertical"></el-divider>
-                        <el-button type="text" @click="deleteMember(scope.row)">删除</el-button>
-                    </template>
-                </el-table-column>
-            </el-table>
-        </el-row>
-        <el-row>
-            <el-pagination background layout="prev, pager, next, jumper"
-                           :total="total" :page-size="pageSize" :current-page="currentPage"
-                           @current-change="currentChange" @size-change="sizeChange" class="pagination">
-            </el-pagination>
-        </el-row>
-        <el-dialog v-if="deleteMemberDialogVisible" title="确认删除成员" :visible.sync="deleteMemberDialogVisible" :before-close="closeDialog">
-            <span>是否确认删除成员 '{{this.selectedMember.username}}'？</span>
-            <span slot="footer" class="dialog-footer">
+  <div class='app-container'>
+    <el-row>
+      <el-col :span='24'>
+        <el-button type='primary' class='float-right' @click="newMember()">添加成员</el-button>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-table v-loading="loading" stripe
+                :data='members.slice((currentPage-1)*pageSize, currentPage*pageSize)'
+                :default-sort="{prop: 'id', order: 'descending'}" style='width: 100%'>
+        <el-table-column prop='id' label='ID' width='180' sortable></el-table-column>
+        <el-table-column label='用户名' width='180' sortable>
+          <template slot-scope="scope">
+            {{scope.row.username}}
+            <span v-if="project && scope.row.username === project.chargedBy">(负责人)</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop='centerName' label='所属中心' sortable></el-table-column>
+        <el-table-column label='任务'>
+          <template slot-scope="scope">{{ taskType[scope.row.task] }}</template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button type="text" @click="assignCenter(scope.row)">分配中心</el-button>
+            <el-divider direction="vertical"></el-divider>
+            <el-button type="text" @click="assignTask(scope.row)">分配任务</el-button>
+            <el-divider direction="vertical"></el-divider>
+            <el-button type="text" @click="deleteMember(scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-row>
+    <el-row>
+      <el-pagination background layout="prev, pager, next, jumper"
+                     :total="total" :page-size="pageSize" :current-page="currentPage"
+                     @current-change="currentChange" @size-change="sizeChange" class="pagination">
+      </el-pagination>
+    </el-row>
+    <el-dialog v-if="deleteMemberDialogVisible" title="确认删除成员"
+               :visible.sync="deleteMemberDialogVisible" :before-close="closeDialog">
+      <span>是否确认删除成员 '{{this.selectedMember.username}}'？</span>
+      <span slot="footer" class="dialog-footer">
                 <el-button @click="deleteMemberDialogVisible = false">取 消</el-button>
                 <el-button type="primary" @click="confirmDelete">删 除</el-button>
             </span>
-        </el-dialog>
-        <member-dialog-component :visible="newMemberDialogVisible" @closeDialog="closeDialog"></member-dialog-component>
-        <assign-member-dialog v-if="assignCenterDialogVisible" :visible="assignCenterDialogVisible" :member-id="selectedMember.id"
-                              @closeDialog="closeDialog"></assign-member-dialog>
-        <assign-task-dialog v-if="assignTaskDialogVisible" :visible="assignTaskDialogVisible" :member-id="selectedMember.id"
-                            @closeDialog="closeDialog"></assign-task-dialog>
-    </div>
+    </el-dialog>
+    <member-dialog-component :visible="newMemberDialogVisible"
+                             @closeDialog="closeDialog"></member-dialog-component>
+    <assign-member-dialog v-if="assignCenterDialogVisible" :visible="assignCenterDialogVisible"
+                          :member-id="selectedMember.id"
+                          @closeDialog="closeDialog"></assign-member-dialog>
+    <assign-task-dialog v-if="assignTaskDialogVisible" :visible="assignTaskDialogVisible"
+                        :member-id="selectedMember.id"
+                        @closeDialog="closeDialog"></assign-task-dialog>
+  </div>
 </template>
 
 <script>
@@ -58,14 +63,16 @@
   import MemberDialogComponent from '../member/MemberDialogComponent'
   import AssignMemberDialog from '../center-member/AssignMemberDialog'
   import AssignTaskDialog from '../member-task/AssignTaskDialog'
+
   export default {
     components: { AssignTaskDialog, AssignMemberDialog, MemberDialogComponent },
     data() {
+      const projectId = this.$route.params.projectId
       return {
         loading: true,
         members: [],
         selectedMember: null,
-        project: { id: 20002, name: 'ALK', open: false, reportId: 20000 },
+        project: { id: projectId, name: null, open: null, reportId: null },
         currentAccount: { id: 4, login: 'user' },
         total: 0,
         pageSize: 10, // 单页数据量
@@ -100,14 +107,15 @@
         })
       },
       getMembers(projectId) {
-        getProjectMembers(projectId, { page: this.currentPage - 1, size: this.pageSize }).then((response) => {
-          this.loading = false
-          this.members = response.data
-          this.total = this.members.length
-        }, (error) => {
-          this.loading = false
-          console.log(error)
-        })
+        getProjectMembers(projectId, { page: this.currentPage - 1, size: this.pageSize }).then(
+          (response) => {
+            this.loading = false
+            this.members = response.data
+            this.total = this.members.length
+          }, (error) => {
+            this.loading = false
+            console.log(error)
+          })
       },
       currentChange: function(currentPage) {
         this.currentPage = currentPage
--- src/renderer/views/project/ProjectQuestionnaire.vue
+++ src/renderer/views/project/ProjectQuestionnaire.vue
@@ -1,57 +1,63 @@
 <template>
-    <div class='app-container'>
-        <el-row>
-            <el-col :span='24'>
-                <el-button type='primary' class='float-right' @click="newQuestionnaire">创建问卷</el-button>
-            </el-col>
-        </el-row>
-        <el-row>
-            <el-table v-loading="loading" stripe :data='questionnaires.slice((currentPage-1)*pageSize, currentPage*pageSize)'
-                      :default-sort = "{prop: 'reportName', order: 'descending'}" height="250" style='width: 100%'>
-                <el-table-column prop='reportName' label='问卷名称' width='180' sortable></el-table-column>
-                <el-table-column prop='lastModifiedDate' label='更新时间' width='180' sortable></el-table-column>
-                <el-table-column align="center">
-                    <template slot="header" slot-scope="scope">
-                        <span>操作</span>
-                    </template>
-                    <template slot-scope="scope">
-                        <el-button type="text" @click="editQuestionnaire(scope.row.id)">编辑</el-button>
-                        <el-divider direction="vertical"></el-divider>
-                        <el-button type="text" @click="deleteQuestionnaire(scope.row)">删除</el-button>
-                        <el-divider direction="vertical"></el-divider>
-                        <el-button type="text" @click="patientInvestigation(scope.row.id)">进行调查</el-button>
-                        <el-divider direction="vertical"></el-divider>
-                        <el-button type="text">调查结果</el-button>
-                        <el-divider direction="vertical"></el-divider>
-                        <el-button type="text">分享调查</el-button>
-                    </template>
-                </el-table-column>
-            </el-table>
-        </el-row>
-        <el-row>
-            <el-pagination background layout="prev, pager, next, jumper"
-                           :total="total" :page-size="pageSize" :current-page="currentPage"
-                           @current-change="currentChange" @size-change="sizeChange" class="pagination">
-            </el-pagination>
-        </el-row>
-        <el-dialog title="确认删除问卷" v-if="deleteDialogVisible" :visible.sync="deleteDialogVisible" :before-close="closeDialog">
-            <span>是否确认删除问卷 '{{selectedQuestionnaire.reportName}}' ？</span>
-            <span slot="footer" class="dialog-footer">
+  <div class='app-container'>
+    <el-row>
+      <el-col :span='24'>
+        <el-button type='primary' class='float-right' @click="newQuestionnaire">创建问卷</el-button>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-table v-loading="loading" stripe
+                :data='questionnaires.slice((currentPage-1)*pageSize, currentPage*pageSize)'
+                :default-sort="{prop: 'reportName', order: 'descending'}" height="250"
+                style='width: 100%'>
+        <el-table-column prop='reportName' label='问卷名称' width='180' sortable></el-table-column>
+        <el-table-column prop='lastModifiedDate' label='更新时间' width='180'
+                         sortable></el-table-column>
+        <el-table-column align="center">
+          <template slot="header" slot-scope="scope">
+            <span>操作</span>
+          </template>
+          <template slot-scope="scope">
+            <el-button type="text" @click="editQuestionnaire(scope.row.id)">编辑</el-button>
+            <el-divider direction="vertical"></el-divider>
+            <el-button type="text" @click="deleteQuestionnaire(scope.row)">删除</el-button>
+            <el-divider direction="vertical"></el-divider>
+            <el-button type="text" @click="patientInvestigation(scope.row.id)">进行调查</el-button>
+            <el-divider direction="vertical"></el-divider>
+            <el-button type="text">调查结果</el-button>
+            <el-divider direction="vertical"></el-divider>
+            <el-button type="text">分享调查</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-row>
+    <el-row>
+      <el-pagination background layout="prev, pager, next, jumper"
+                     :total="total" :page-size="pageSize" :current-page="currentPage"
+                     @current-change="currentChange" @size-change="sizeChange" class="pagination">
+      </el-pagination>
+    </el-row>
+    <el-dialog title="确认删除问卷" v-if="deleteDialogVisible" :visible.sync="deleteDialogVisible"
+               :before-close="closeDialog">
+      <span>是否确认删除问卷 '{{selectedQuestionnaire.reportName}}' ？</span>
+      <span slot="footer" class="dialog-footer">
                 <el-button @click="deleteDialogVisible = false">取 消</el-button>
                 <el-button type="primary" @click="confirmDelete">删 除</el-button>
             </span>
-        </el-dialog>
-    </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
   import { getProjectQuestionnaires, deleteQuestionnaire } from '@/api/QuestionnaireService'
+
   export default {
     name: 'ProjectQuestionnaire',
     data() {
+      const projectId = this.$route.params.projectId
       return {
         loading: true,
-        projectId: 20002,
+        projectId: projectId,
         questionnaires: [],
         selectedQuestionnaire: null,
         total: 0,
