f18b140775c0a8cdced7bb05b8ffc58d715d7f7e
yuanzhencai <zhencai.yuan@datarx.cn>
2020-03-11 09:59:59 +0800

    feat: hasAnyAuthority

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,31 +1,26 @@
 import Vue from 'vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import 'normalize.css/normalize.css'// A modern alternative to CSS reset
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS reset
 import 'bootstrap/dist/css/bootstrap.css'
-import moment from 'moment'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
-
 import App from './App'
 import router from './router'
 import store from './store'
 
 import '@/icons' // icon
 import '@/permission' // permission control
+import * as config from './shared/config/config'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 
-Vue.use(VueAxios, axios)
 Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
-Vue.prototype.$axios = axios
-Vue.filter('formatDate', function(val, pattern) {
-  return moment(val).format(pattern)
-})
+
+config.initVueApp(Vue)
 
 new Vue({
   components: { App },
