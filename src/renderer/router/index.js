32219f33cf059382b3642388cc55a42c38e67727
jiangyaning <15038588199@163.com>
2020-03-13 13:23:52 +0800

    切换分支解决logging Bug

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -24,7 +24,6 @@ import Layout from '../views/layout/Layout'
 export const constantRouterMap = [
   { path: '/login', component: () => import('@/views/login/index'), hidden: true },
   { path: '/404', component: () => import('@/views/404'), hidden: true },
-
   {
     path: '/',
     component: Layout,
@@ -34,9 +33,9 @@ export const constantRouterMap = [
     children: [{
       path: 'dashboard',
       component: () => import('@/views/dashboard/index')
-    }]
+    },
+    { path: '/settings', component: () => import('@/views/user-setting/userSettings'), hidden: true }]
   },
-
   {
     path: '/example',
     component: Layout,
