89c055568a42a710ca0e10862d67b7f58f477d51
袁振才 <zhencai.yuan@datarx.cn>
2020-09-04 18:58:40 +0800

    feat: 二级域名

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://reg.palandata.com/"',
+  BASE_API: '"https://reg.palandata.com/"',
   AGORA_ID: '"d17d26b18b574c7c85611c912b5401ce"'
 }
