9cf4b2a20fcfd868fab66b2592398683dc38e8af
jiangyaning <15038588199@163.com>
2021-05-08 14:07:23 +0800

    H5跳转小程序修改

--- src/renderer/views/h5/PatientIncorporation.vue
+++ src/renderer/views/h5/PatientIncorporation.vue
@@ -51,11 +51,10 @@
 		</el-form>
 	</div>
 </template>
-<script type="text/javascript" src="https://res.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
 <script>
 	import { getIncorporationPatient, incorporationPatient } from '../../api/IncorporationService'
 	import { getProject } from '@/api/ProjectService'
-	import wx from 'weixin-js-sdk'
+  import wx from 'weixin-js-sdk'
 
 	const codes = require('../patient/pca-code.json')
 
@@ -123,7 +122,7 @@
 	          incorporationPatient(this.patient).then(res => {
 	            this.isSaving = false
 	            this.getIncorporationPatient()
-	            this.ToHomePage()
+              this.ToHomePage()
 	          }, () => {
 	            this.isSaving = false
 	          })
@@ -135,11 +134,11 @@
 	      this.patient.city = value[1]
 	      this.patient.area = value[2]
 	    },
-	    ToHomePage() {
-	      wx.switchTab({
-	        url: '/pages/template/home/overview'
-	      })
-	    }
+      ToHomePage() {
+        wx.miniProgram.switchTab({
+          url: '/pages/template/home/overview'
+        })
+      }
 	  }
 	}
 </script>
