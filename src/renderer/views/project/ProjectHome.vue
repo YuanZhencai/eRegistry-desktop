e2b6cd402a02b4cd4d4bfb7a4d061d94ddf9c4ac
jiangyaning <15038588199@163.com>
2021-05-14 18:04:03 +0800

    修改首页告警样式

--- src/renderer/views/project/ProjectHome.vue
+++ src/renderer/views/project/ProjectHome.vue
@@ -1,31 +1,44 @@
 <template>
   <div class="col-flex h100">
-    <el-row class="no-flex">
-      <div class="float-left row-flex">
-        <div class="avatar" style="margin-right: 15px;">
-          <user-avatar :size="50" :username="name"></user-avatar>
+    <el-row type="flex" class="row-bg" justify="space-between">
+      <el-col :span="6">
+        <div class="grid-content bg-purple">
+          <div class="float-left row-flex">
+            <div class="avatar" style="margin-right: 15px;">
+              <user-avatar :size="50" :username="name"></user-avatar>
+            </div>
+            <div class="desc">
+              <p><small>当前登录用户</small></p>
+              <div class="desc-title fa-lg" v-if="name"><strong>{{name}}</strong></div>
+            </div>
+          </div>
         </div>
-        <div class="desc">
-          <p><small>当前登录用户</small></p>
-          <div class="desc-title fa-lg" v-if="name"><strong>{{name}}</strong></div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple-light" style="margin-top: 12px;font-size: 20px">
+          <strong>{{this.projectName}}</strong>
         </div>
-      </div>
-      <div class="float-right row-flex" v-if="statistics && statistics.total && statistics.yesterday">
-        <div>
-          <p class="no-margin-bottom"><small>患者数</small></p>
-          <p><span class="fa-2x">{{statistics.total.patientCount}}</span></p>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple">
+          <div class="float-right row-flex" v-if="statistics && statistics.total && statistics.yesterday">
+            <div>
+              <p class="no-margin-bottom"><small>患者数</small></p>
+              <p><span class="fa-2x">{{statistics.total.patientCount}}</span></p>
+            </div>
+            <el-divider direction="vertical" class="height-divider"></el-divider>
+            <div>
+              <p class="no-margin-bottom"><small>昨日新增病例</small></p>
+              <p><span class="fa-2x">{{statistics.yesterday.patientCaseCount}}<span> / {{statistics.total.patientCaseCount}}</span></span></p>
+            </div>
+            <el-divider direction="vertical" class="height-divider"></el-divider>
+            <div>
+              <p class="no-margin-bottom"><small>昨日新增随访</small></p>
+              <p><span class="fa-2x">{{statistics.yesterday.followCount}}<span> / {{statistics.total.followCount}}</span></span></p>
+            </div>
+          </div>
         </div>
-        <el-divider direction="vertical" class="height-divider"></el-divider>
-        <div>
-          <p class="no-margin-bottom"><small>昨日新增病例</small></p>
-          <p><span class="fa-2x">{{statistics.yesterday.patientCaseCount}}<span> / {{statistics.total.patientCaseCount}}</span></span></p>
-        </div>
-        <el-divider direction="vertical" class="height-divider"></el-divider>
-        <div>
-          <p class="no-margin-bottom"><small>昨日新增随访</small></p>
-          <p><span class="fa-2x">{{statistics.yesterday.followCount}}<span> / {{statistics.total.followCount}}</span></span></p>
-        </div>
-      </div>
+      </el-col>
     </el-row>
     <el-divider class="no-flex no-margin-top"></el-divider>
     <div class="flex-grow" style="position: relative;">
@@ -70,16 +83,34 @@
             </el-row>
           </el-col>
           <el-col :span="8" :offset="1">
-            <el-card shadow="hover" class="panel-group">
-              <div slot="header" class="clearfix">
-                <span>快速开始 / 便捷导航</span>
+            <el-card class="box-card panel-group">
+              <div slot="header" class="clearfix" style="margin-top: 10px;">
+                <span slot="label">告警<el-badge :value="7" class="item"></el-badge></span>
               </div>
-              <div class="card-body">
-                <el-button type="text" @click="setProject"
-                           v-if="$hasAnyAuthority(['PROJECT_ADMIN_' + projectId, 'PROJECT_MASTER_' + projectId])">项目设置</el-button>
-                <el-button type="text"
-                           v-if="$hasAnyAuthority(['PROJECT_ADMIN_' + projectId, 'PROJECT_PATIENT_' + projectId])">
-                  <router-link :to="{ path: `/project/${projectId}/report`, params: {projectId} }">CRF</router-link>
+              <el-table
+                :data="tableData"
+                :show-header="false"
+                stripe
+                style="width: 100%">
+                <el-table-column>
+                  <template slot-scope="scope">
+                    <el-row>
+                      <el-col :span="12"><div class=" " style="font-size: 18px;font-weight: 500;">不良反应</div></el-col>
+                      <el-col :span="12"><div class=" " style="text-align: right;"><el-button type="text">查看</el-button></div></el-col>
+                    </el-row>
+                    <el-row>
+                      <el-col :span="24"><div class=" " style="white-space: nowrap;text-overflow: ellipsis;overflow: hidden;">食欲下降很严重、食欲下降很严重、食欲下降很严重、食欲下降很严重、食欲下降很严重、食欲下降很严重、食欲下降很严重、食欲下降很严重、食欲下降很严重、食欲下降很严重、食欲下降很严重、食欲下降很严重、</div></el-col>
+                    </el-row>
+                    <el-row>
+                      <el-col :span="12"><div class=" " style="color: #6c757d;margin-top: 5px;">刘忻好</div></el-col>
+                      <el-col :span="12"><div class=" " style="text-align: right">2021-05-14</div></el-col>
+                    </el-row>
+                  </template>
+                </el-table-column>
+              </el-table>
+              <div class="text-right" v-if="changes.length > 5">
+                <el-button type="text" size="mini">
+                  <router-link :to="{ path: `/project/${projectId}/log`, params: {projectId} }">更多...</router-link>
                 </el-button>
               </div>
             </el-card>
@@ -106,7 +137,20 @@
                 </div>
               </div>
             </el-card>
-            <el-card shadow="hover" :body-style="{ padding: '10px' }">
+            <el-card shadow="hover" class="panel-group" :body-style="{ padding: '10px'}">
+              <div slot="header" class="clearfix">
+                <span>快速开始 / 便捷导航</span>
+              </div>
+              <div class="card-body">
+                <el-button type="text" @click="setProject"
+                           v-if="$hasAnyAuthority(['PROJECT_ADMIN_' + projectId, 'PROJECT_MASTER_' + projectId])">项目设置</el-button>
+                <el-button type="text"
+                           v-if="$hasAnyAuthority(['PROJECT_ADMIN_' + projectId, 'PROJECT_PATIENT_' + projectId])">
+                  <router-link :to="{ path: `/project/${projectId}/report`, params: {projectId} }">CRF</router-link>
+                </el-button>
+              </div>
+            </el-card>
+            <el-card shadow="hover" :body-style="{ padding: '10px'}">
               <div slot="header" class="clearfix">
                 <span>动态</span>
               </div>
@@ -147,6 +191,7 @@
   import { getProjectStatistics, getDateChart, getPlanChart } from '@/api/StatisticsService'
   import UserAvatar from '../../components/avatar/userAvatar'
   import ProjectDialogComponent from './ProjectDialogComponent'
+  import { getProject } from '@/api/ProjectService'
 
   export default {
     name: 'ProjectHome',
@@ -186,13 +231,43 @@
           AUDIT: '审核',
           VIEW: '查看',
           ADMIN: '负责人'
-        }
+        },
+        projectName: '',
+        activeName: 'first',
+        tableData: [{
+          questionnaire: '不良反应',
+          name: '示长星',
+          bad: '食欲下降很严重',
+          date: '2021-05-10'
+        }, {
+          questionnaire: '不良反应',
+          name: '虢昆鹏',
+          bad: '胃灼热严重',
+          date: '2021-05-10'
+        }, {
+          questionnaire: '卵巢癌患者随访项目不良反应',
+          name: '刘星',
+          bad: '食欲下降很严重',
+          date: '2021-05-10'
+        }]
       }
     },
     created() {
       this.loadAll()
     },
     methods: {
+      handleClick(tab, event) {
+      },
+      view1() {
+        this.$router.push({
+          path: `/project/20058/questionnaire/51/investigation/186`
+        })
+      },
+      view2() {
+        this.$router.push({
+          path: `/project/20058/questionnaire/51/investigation/187`
+        })
+      },
       loadAll() {
         this.findCenters()
         this.findProjectStatistics()
@@ -200,6 +275,7 @@
         this.plan()
         this.findMembers()
         this.findChanges()
+        this.getProjects()
       },
       findCenters() {
         getAllCenters({ 'EQ_center.projectId': this.projectId }).then((res) => {
@@ -242,6 +318,12 @@
         this.$refs['project-dialog'].show(this.projectId).then(() => {
           this.loadAll()
         }, () => {})
+      },
+      getProjects() {
+        getProject(this.projectId).then(res => {
+          this.projectName = res.data.name
+          console.log(this.projectName, '2')
+        })
       }
     }
   }
